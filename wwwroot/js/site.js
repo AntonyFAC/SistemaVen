79f48fb53fef8a61caf60d4203825c13286e7c21
AntonyFAC <katochiac@gmail.com>
2022-06-11 11:16:57 -0500

    Mensaje flotante

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.querySelector(".first").addEventListener('click', function(){
+    Swal.fire("Se Agrego el Producto");
+  });
